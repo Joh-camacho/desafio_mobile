1675743816e76c02bf4b5f46da550bc7bd0cf1bb
Johnny Camacho <joh_camacho@outlook.com>
2022-11-28 23:06:25 -0300

    feat: implementing map in Home screen

--- desafio_mobile/desafio_mobile/Views/Home/HomeViewController.swift
+++ desafio_mobile/desafio_mobile/Views/Home/HomeViewController.swift
@@ -6,12 +6,69 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        locationManager.distanceFilter = 10
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        return locationManager
+    }()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+    }
+}
+
+// MARK: - Private functions
+extension HomeViewController {
+    
+    private func updateMap(_ location: CLLocation) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
         
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+// MARK: - LocationManagerDelegate
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.requestLocation()
+            
+            mapView.showsUserLocation = true
+        case .restricted, .denied:
+            // TODO: Present alert error
+            fatalError("Autorização negada.")
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        @unknown default:
+            // TODO: Present alert error
+            fatalError()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        
+        updateMap(location)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // TODO: Present alert error
+        print("[MapViewController] didFailWithError(error: \(error))")
     }
 }
