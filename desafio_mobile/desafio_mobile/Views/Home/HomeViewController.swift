97cbc2cc46023862607e22b22f1ed653e42a8344
Johnny Camacho <joh_camacho@outlook.com>
2022-11-28 22:05:46 -0300

    feat: create Home and Login view controller

---
+++ desafio_mobile/desafio_mobile/Views/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  desafio_mobile
+//
+//  Created by Johnny Camacho on 28/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
--- desafio_mobile/desafio_mobile/Views/Login/LoginViewController.swift
+++ desafio_mobile/desafio_mobile/Views/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  desafio_mobile
 //
 //  Created by Johnny Camacho on 28/11/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
-
-
 }
-
